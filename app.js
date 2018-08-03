a257bb392bb3f5dbc6ddbfb881a6ca0c95f7f498
Isaiah White <isaiahwhite208@gmail.com>
2018-08-03 16:55:27 -0600

    push

--- app.js
+++ app.js
@@ -1,51 +1,20 @@
 let health = 100
 
-const slap = {
- name: "slap",
- damage: 1,
- mods: [
-  "shield",
-  "slag",
- ],
-}
-const punch = {
- name: "punch",
- damage: 5,
- mods: [
-  "shield",
-  "slag",
- ],
-}
-const kick = {
- name: "kick",
- damage: 10,
- mods: [
-  "shield",
-  "slag",
- ],
-}
-const shield = {
- name: "shield",
- damage: 10, //reduces half incoming dmg
-}
-const slag = {
- name: "slag",
- damage: 10, //deals 1.5 dmg
-}
-const heal = {
- name: "heal",
- damage: 10, //gives 25 HP
+function draw() {
+ health
 }
 
-let damage = [slap, kick, punch]
-
-// function draw() {
-
-// }
-
-function damage() {
- let slap = 0
-
+function slap() {
+ health -= 1
+ alert(health)
 }
 
-alert(health)
+function kick() {
+ health -= 10
+ alert(health)
+}
+
+function punch() {
+ health -= 5
+ alert(health)
+}
