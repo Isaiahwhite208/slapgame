250a285d892e6ea5f6f67ad861b86b78f1f1288e
Isaiah White <isaiahwhite208@gmail.com>
2018-08-03 15:30:58 -0600

    push

---
+++ app.js
@@ -0,0 +1,26 @@
+let health = 100
+
+const slap = {
+ name: "slap",
+ damage: 1,
+ mods: [
+  "shield",
+  "slag",
+ ],
+}
+const punch = {
+ name: "punch",
+ damage: 1,
+ mods: [
+  "shield",
+  "slag",
+ ],
+}
+const kick = {
+ name: "kick",
+ damage: 1,
+ mods: [
+  "shield",
+  "slag",
+ ],
+}
