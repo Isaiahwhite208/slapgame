74768d0caef554e80c5fe1fd9732ba5d435fe937
Isaiah White <isaiahwhite208@gmail.com>
2018-08-03 21:57:06 -0600

    layout change and figured out element by ID

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 let health = 100
+let hits = 0
+
+let mods = {
+ shield: {
+  modifier: .5,
+ },
+ amp: {
+  modifier: 1.5
+ },
+ heal: {
+  modifier: -5
+ }
+}
 
 function draw() {
- health
+ if (health <= 0) {
+  health = 0
+ }
+ document.getElementById("health").innerText = " " + health;
+ document.getElementById("hits").innerText = " " + hits++;
 }
 
 function slap() {
  health -= 1
+ hits += 0
  draw()
 }
 
 function kick() {
  health -= 10
+ hits += 0
  draw()
 }
 
 function punch() {
  health -= 5
+ hits += 0
  draw()
 }
+
+function shield() {
+ mods.shield
+}
+
+function amp() {
+ mods.amp
+}
+
+function heal() {
+ mods.heal
+}
+
+draw()
